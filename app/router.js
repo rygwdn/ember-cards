7642c31963a364142ac35d780c66bfc9ecf553c1
Ryan Wooden <rygwdn@gmail.com>
2015-01-10 15:37:04 -0400

    add players and cards.

--- app/router.js
+++ app/router.js
@@ -1,11 +1,13 @@
-import Ember from 'ember';
-import config from './config/environment';
+import Ember from "ember";
+import config from "./config/environment";
 
 var Router = Ember.Router.extend({
   location: config.locationType
 });
 
 Router.map(function() {
+  this.resource("cards", function() {});
+  this.resource("players", function() {});
 });
 
 export default Router;
