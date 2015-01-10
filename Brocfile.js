9b8f516d0bd5163163d5326bb03a52e4e37e3d18
Ryan Wooden <rygwdn@gmail.com>
2015-01-10 15:35:48 -0400

    add dragdrop

--- Brocfile.js
+++ Brocfile.js
@@ -23,4 +23,8 @@ var app = new EmberApp({
 // please specify an object with the list of modules as keys
 // along with the exports of each module as its value.
 
+app.import('bower_components/modernizr/modernizr.js');
+app.import('bower_components/draggabilly/dist/draggabilly.pkgd.js');
+app.import('vendor/dragdrop.js');
+
 module.exports = app.toTree();
