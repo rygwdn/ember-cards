e8ebc45f81d8633cae5a50aa18f45c8499251c6c
Ryan Wooden <rygwdn@gmail.com>
2015-01-12 07:43:40 -0400

    add display to players

--- app/models/player.js
+++ app/models/player.js
@@ -1,5 +1,10 @@
 import DS from 'ember-data';
+import Ember from 'ember';
 
 export default DS.Model.extend({
   name: DS.attr('string'),
+  email: DS.attr('string'),
+  display: Ember.computed('name', 'email', function() {
+    return this.get('name') + ' (' + this.get('email') + ')';
+  }),
 });
